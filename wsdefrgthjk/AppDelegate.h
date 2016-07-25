72f5c538128c59dbdb04813f1166abe2a367a58f
liangweiweiwei <mac@liang.local>
2016-07-25 14:43:16 +0800

    liang
    add

---
+++ wsdefrgthjk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wsdefrgthjk
+//
+//  Created by Mac on 16/7/25.
+//  Copyright © 2016年 梁伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wsdefrgthjk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wsdefrgthjk
+//
+//  Created by Mac on 16/7/25.
+//  Copyright © 2016年 梁伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
